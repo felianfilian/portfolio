5474e6b0075e9779e2b69b1becf38818ca76b426
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-22 12:55:50 +0100

    burger menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  showMenu() {
+    alert('show');
+    let mainMenu = document.getElementById('main-menu');
+    if (mainMenu != null) {
+      mainMenu.style.display = 'flex';
+    }
+  }
 }
