a623f028fbfd315444287abb2107ba6f6c2c0a5b
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-24 00:13:19 +0100

    main update

--- src/app/section05-contact/section05-contact.component.ts
+++ src/app/section05-contact/section05-contact.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-section05-contact',
   templateUrl: './section05-contact.component.html',
-  styleUrls: ['./section05-contact.component.scss']
+  styleUrls: ['./section05-contact.component.scss'],
 })
 export class Section05ContactComponent {
-
+  sendMail() {
+    alert('Mail sended');
+  }
 }
