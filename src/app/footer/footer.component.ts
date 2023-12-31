4b0b4a4f1e9e7c91a9ef870753a539ecd6fe6227
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-12-31 20:51:36 +0100

    final alpha beta

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   thisYear: number = new Date().getFullYear();
+  lang: string = 'eng';
+
+  constructor(public gs: GlobalService) {
+    this.lang = gs.lang;
+  }
+
+  showImprint() {
+    let mobileMenu = document.getElementById('imprint-container');
+    if (mobileMenu != null) {
+      mobileMenu.style.display = 'flex';
+    }
+  }
+  closeImprint() {
+    let mobileMenu = document.getElementById('imprint-container');
+    if (mobileMenu != null) {
+      mobileMenu.style.display = 'none';
+    }
+  }
 }
